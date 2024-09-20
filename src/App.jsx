5aac92fb54a367e02252a221356065be9bd2736b
juber131 <jubayerahmad131@gmail.com>
2024-09-20 11:50:55 +0600

    add image

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import { FaHome } from "react-icons/fa";
 import "../src/component/Custom.scss"
+import "../src/component/new.scss"
 
 function App() {
   return (
@@ -61,6 +62,108 @@ function App() {
         </div>
       </div>
       <FaHome className="add" /><FaHome className="text-primary" /><FaHome className="text-primary" />
+
+      <div id="carouselExampleDark" className="carousel carousel-dark slide" data-bs-ride="carousel">
+        <div className="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active" data-bs-interval="10000">
+            <img src="https://i.ibb.co.com/c8mHxJK/IMG-20231203-WA0111-removebg-preview.png" className="d-block h-80 w-80" alt="..." />
+
+            <div className="carousel-caption d-none d-md-block">
+              <h5>First slide label</h5>
+              <p>Some representative placeholder content for the first slide.</p>
+            </div>
+          </div>
+          <div className="carousel-item" data-bs-interval="2000">
+            <img src="https://i.ibb.co.com/c8mHxJK/IMG-20231203-WA0111-removebg-preview.png" className="d-block w-80" alt="..." />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>Second slide label</h5>
+              <p>Some representative placeholder content for the second slide.</p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img src="https://i.ibb.co.com/c8mHxJK/IMG-20231203-WA0111-removebg-preview.png" className="d-block w-80" alt="..." />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>Third slide label</h5>
+              <p>Some representative placeholder content for the third slide.</p>
+            </div>
+          </div>
+        </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+
+      <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
+        <div className="carousel-inner">
+          <div className="carousel-item active" data-bs-interval="10000">
+            <img src="https://i.ibb.co.com/c8mHxJK/IMG-20231203-WA0111-removebg-preview.png" className="d-block w-80 h-80" alt="..." />
+          </div>
+          <div className="carousel-item" data-bs-interval="2000">
+            <img src="https://i.ibb.co.com/c8mHxJK/IMG-20231203-WA0111-removebg-preview.png" className="d-block w-80 h-80" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src="https://i.ibb.co.com/c8mHxJK/IMG-20231203-WA0111-removebg-preview.png" className="d-block w-80 h-80" alt="..." />
+          </div>
+        </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+
+      <div>
+        <h1 className="text-center bold ">jubaer </h1>
+      </div>
+      <nav className="navbar mb-50 navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Navbar</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Link</a>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a className="dropdown-item" href="#">Action</a></li>
+                  <li><a className="dropdown-item" href="#">Another action</a></li>
+                  <li><hr className="dropdown-divider" /></li>
+                  <li><a className="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled" href="#" aria-disabled="true">Disabled</a>
+              </li>
+            </ul>
+            <form className="d-flex">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+              <button className="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
     </>
   );
 }
